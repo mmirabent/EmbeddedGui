6e60e4a0e893dad6bb6b48c5f0954ba8fa7a80a4
Marcos Mirabent <marcos@mirabent.net>
2016-01-28 10:54:57 -0500

    Makefile improvements, exception handling
    Makefile improvements, gitignoring executable files, exception handling.
    Also added using namespace statement to make code cleaner.

--- 2dPirates/main.cc
+++ 2dPirates/main.cc
@@ -2,55 +2,67 @@
 #include <string>
 #include <cstdlib>
 
+using namespace std;
+
 int main() {
 
     // Local variables
     bool done = false;
     int x, y, i, j;
     int** myArray;
-    std::string welcome_msg = "Welcome to the 2D Array Exercise";
-    std::string rows_msg = "How many rows you would like: ";
-    std::string cols_msg = "How many columns you would like: ";
-    std::string repeat_msg = "Would you like to create another array [Y/n]? ";
-    std::string another_one = "";
+    string welcome_msg  = "Welcome to the 2D Array Exercise";
+    string rows_msg     = "How many rows you would like: ";
+    string cols_msg     = "How many columns you would like: ";
+    string repeat_msg   = "Would you like to create another array [Y/n]? ";
+    string alloc_error  = "Not enough memory for an array of that size.";
+    string number_error = "Please enter positive numbers of rows and columns";
+    string another_one  = "";
 
     // Display a nice welcome message
-    std::cout << welcome_msg << std::endl;
+    cout << welcome_msg << endl;
 
     // Loop until the user says so
     while(!done) {
 
+        // Reset x and y
+        x = 0;
+        y = 0;
+
         // Get x and y from user
-        std::cout << rows_msg;
-        std::cin >> x;
-        std::cout << cols_msg;
-        std::cin >> y;
+        cout << rows_msg;
+        cin >> x;
+        cout << cols_msg;
+        cin >> y;
 
         // Check x and y for correctness
         if(x <= 0 || y <= 0) {
-            std::cout << "Please enter positive numbers of rows and columns\n";
+            cout << number_error << endl;
             continue;
         }
 
-        // Allocate a 2d array
-        myArray = new int*[x];
-        for(i = 0; i < x; i++) {
-            myArray[i] = new int[y];
-        }
-
-        // Fill the array 
-        for(i = 0; i < x; i++) {
-            for(j = 0; j < y; j++) {
-                myArray[i][j] = rand() % 100 + 1; // Random numbers 1-100
+        try {
+            // Allocate a 2d array
+            myArray = new int*[x];
+            for(i = 0; i < x; i++) {
+                myArray[i] = new int[y];
             }
-        }
 
-        // Print the array
-        for(i = 0; i < x; i++) {
-            for(j = 0; j < y; j++) {
-                std::cout << myArray[i][j] << " ";
+            // Fill the array 
+            for(i = 0; i < x; i++) {
+                for(j = 0; j < y; j++) {
+                    myArray[i][j] = rand() % 100 + 1; // Random numbers 1-100
+                }
             }
-            std::cout << std::endl;
+
+            // Print the array
+            for(i = 0; i < x; i++) {
+                for(j = 0; j < y; j++) {
+                    cout << myArray[i][j] << " ";
+                }
+                cout << endl;
+            }
+        } catch (const bad_alloc& e) {
+            cout << alloc_error << endl;
         }
 
         // Delete the array
@@ -60,8 +72,8 @@ int main() {
         delete[] myArray;
 
         // Ask if the user want's another array
-        std::cout << repeat_msg;
-        std::cin >> another_one;
+        cout << repeat_msg;
+        cin >> another_one;
 
         if(another_one == "n" || another_one == "N") {
             done = true;
