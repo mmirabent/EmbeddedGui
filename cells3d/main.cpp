8052c4e782dc46f47fbf4789e9fe6d83744958d7
Marcos Mirabent <marcos@mirabent.net>
2016-02-03 09:46:48 -0500

    Made the 3d array of cells not ints

--- cells3d/main.cpp
+++ cells3d/main.cpp
@@ -10,28 +10,20 @@ int main() {
     // Local variables
     bool done = false;
     int x, y, z, i, j, k;
-    int*** myArray;
-    string welcome_msg  = "Welcome to the 3D Array Exercise";
-    string rows_msg     = "How many rows you would like: ";
-    string cols_msg     = "How many columns you would like: ";
-    string height_msg   = "How many layers would you like: ";
-    string repeat_msg   = "Would you like to create another array [Y/n]? ";
+    Cell ****myArray;
+    string welcome_msg = "Welcome to 3D Cells Exercise";
+    string rows_msg = "X: ";
+    string cols_msg = "Y: ";
+    string height_msg = "Z: ";
+    string repeat_msg = "Would you like to create another array [Y/n]? ";
     string number_error = "Please enter positive numbers";
-    string another_one  = "";
+    string another_one = "";
 
     // Display a nice welcome message
     cout << welcome_msg << endl;
 
-    Cell* foo = new Cell();
-    foo->loc.x = 10;
-    foo->loc.y = 20;
-    foo->loc.z = 30;
-    foo->tick();
-
-    delete foo;
-
     // Loop until the user says so
-    while(!done) {
+    while (!done) {
 
         // Reset x and y
         x = 0;
@@ -47,47 +39,49 @@ int main() {
         cin >> z;
 
         // Check x, y, and z for correctness
-        if(x <= 0 || y <= 0 || z <= 0) {
+        if (x <= 0 || y <= 0 || z <= 0) {
             cout << number_error << endl;
             continue;
         }
 
         // Allocate a 3d array
-        myArray = new int**[x];
-            for(i = 0; i < x; i++) {
-                myArray[i] = new int*[y];
-                for(j = 0; j < y; j++) {
-                    myArray[i][j] = new int[z];
+        myArray = new Cell ***[z];
+        for (k = 0; k < z; k++) {
+            myArray[k] = new Cell **[y];
+            for (j = 0; j < y; j++) {
+                myArray[k][j] = new Cell *[x];
+            }
+        }
+
+        // Fill the array
+        for (k = 0; k < z; k++) {
+            for (j = 0; j < y; j++) {
+                for (i = 0; i < x; i++) {
+                    myArray[k][j][i] = new Cell();
                 }
             }
+        }
 
-            // Fill the array 
-            for(i = 0; i < x; i++) {
-                for(j = 0; j < y; j++) {
-                    for(k = 0; k < z; k++) {
-                        myArray[i][j][k] = rand() % 100 + 1; // Random numbers 1-100
-                    }
-                }
-            }
+        cout << endl;
 
-            // Print the array
-            for(k = 0; k < z; k++) {
-                cout << "Z: " << k << endl;
-                for(i = 0; i < x; i++) {
-                    for(j = 0; j < y; j++) {
-                        cout << myArray[i][j][k] << " ";
-                    }
-                    cout << endl;
+        // Print the array
+        for (k = 0; k < z; k++) {
+            cout << "Z: " << k << endl;
+            for (j = 0; j < y; j++) {
+                for (i = 0; i < x; i++) {
+                    myArray[k][j][i]->tick();
                 }
                 cout << endl;
             }
+            cout << endl;
+        }
 
         // Delete the array
-        for(i = 0; i < x; i++) {
-            for(j = 0; j < y; j++) {
-                delete[] myArray[i][j];
+        for (k = 0; k < z; k++) {
+            for (j = 0; j < y; j++) {
+                delete[] myArray[k][j];
             }
-            delete[] myArray[i];
+            delete[] myArray[k];
         }
         delete[] myArray;
 
@@ -95,11 +89,9 @@ int main() {
         cout << repeat_msg;
         cin >> another_one;
 
-        if(another_one == "n" || another_one == "N") {
+        if (another_one == "n" || another_one == "N") {
             done = true;
         }
 
     } // While
 } // Main
-
-
